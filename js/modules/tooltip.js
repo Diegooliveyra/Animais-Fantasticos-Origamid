486afe6dcd396221784ae99830c4ef78c2ba2a7e
diegooliveyra <diegooliveirabryto@gmail.com>
2021-01-12 10:03:52 -0300

    refatorado a tooltip para classe

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,6 +1,6 @@
 export default class initTooltip {
   constructor(tooltips) {
-    this.tooltips = document.querySelectorAll(tooltips);
+    this.tooltip = document.querySelectorAll(tooltips);
 
     // bind do obj da classe aos callbacks
     this.onMouseLeave = this.onMouseLeave.bind(this);
@@ -8,12 +8,18 @@ export default class initTooltip {
     this.onMouseOver = this.onMouseOver.bind(this);
   }
 
+  // Move a tooltip com base em seus estilos de acordo com a posição do mouse 
   onMouseMove(event) {
     this.tooltipBox.style.top = `${event.pageY + 20} px`;
-    this.tooltipBox.style.left = `${event.pageX + 20} px`;
+    if (event.pageX + 240 > window.innerWidth) {
+      this.tooltipBox.style.left = `${event.pageX - 190} px`;
+    } else {
+      this.tooltipBox.style.left = `${event.pageX + 20} px`;
+    }
   }
 
-  onMouseLeave({currentTarget}) {
+  // remove a tooltip e os eventos de mousemove e mouseleave
+  onMouseLeave({ currentTarget }) {
     this.tooltipBox.remove();
     currentTarget.removeEventListener("mouseleave", this.onMouseLeave);
     currentTarget.removeEventListener("mousemove", this.onMouseMove);
@@ -29,21 +35,23 @@ export default class initTooltip {
     this.tooltipBox = tooltipBox;
   }
 
-  onMouseOver({currentTarget}) {
+// cria a tooltip  e adciona os eventos de mouseleave e mousemove ao target
+  onMouseOver({ currentTarget }) {
     // cria a tooltip box e coloca em uma propriedade
     this.criarTooltipBox(currentTarget);
     currentTarget.addEventListener("mousemove", this.onMouseMove);
     currentTarget.addEventListener("mouseleave", this.onMouseLeave);
   }
 
+  // adiciona os eventos de mouseover a casa tolltip
   addTooltipEvents() {
-    this.tooltips.forEach((item) => {
+    this.tooltip.forEach((item) => {
       item.addEventListener("mouseover", this.onMouseOver);
     });
   }
 
   init() {
-    if (this.tooltips.length) {
+    if (this.tooltip.length) {
       this.addTooltipEvents();
     }
 
